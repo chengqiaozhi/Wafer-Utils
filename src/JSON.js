d8e930178919740c7ab7a53c9890ef2c45792f59
chengqiaozhi <Qz951013>
2024-03-12 16:05:33 +0800

    test

---
+++ src/JSON.js
@@ -0,0 +1,14 @@
+// date.js
+
+function formatDate(date, format) {
+    // ... 实现日期格式化 ...
+    return 111;
+}
+
+function addDays(date, days) {
+    // ... 实现添加天数 ...
+    return 222;
+}
+
+// 导出函数
+export { formatDate, addDays };
